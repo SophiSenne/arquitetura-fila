a7528c94f7827a9e231219d162ab87a8646d783c
SophiSenne <Sophia.Silva@sou.inteli.edu.br>
2026-03-21 18:52:38 -0300

    fix: corrige logica do back-end

--- back-end/main.go
+++ back-end/main.go
@@ -1,20 +1,22 @@
 package main
- 
+
 import (
 	"log"
- 
-	"sensor-api/router"
+
 	"sensor-api/messaging"
+	"sensor-api/router"
 )
- 
+
 func main() {
 	pool, err := messaging.GetPool()
-	if err != nil{
-		r := router.Setup()
-		if err := r.Run(":8080"); err != nil {
-			log.Fatal(err)
-		}
+	if err != nil {
+		log.Printf("[main] Aviso: pool RabbitMQ não inicializado: %v", err)
+	} else {
 		defer pool.Close()
 	}
-	
+
+	r := router.Setup()
+	if err := r.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
 }
